f8e5641890ff56ee534a0dfbccc57e45d42a0ef6
smashell <shanghai1980@gmail.com>
2025-10-06 16:56:11 +0800

    group pkgs

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -1,91 +1,70 @@
 { pkgs }:
 
 with pkgs; [
-  # # General packages for development and system management
-  # alacritty
-  # aspell
-  # aspellDicts.en
+  # System & Terminal Utilities
   bash-completion
   bat
-  btop
   coreutils
   killall
-  # neofetch
-  fastfetch
-  openssh
-  # sqlite
   wget
   zip
   xz
+  unzip
+  p7zip
+  unrar
+  openssh
+  fd
+  fzf
+  zoxide
   just
-  # meson
-  # ninja
+  tree
+  htop
+  btop
+  iftop
+  duf
+
+  # Development & Data Processing
   glow
+  jq
+  yq-go # yaml processer https://github.com/mikefarah/yq
+  difftastic
   ansible
+  uv
 
-  # Encryption and security tools
-  age
-  age-plugin-yubikey
-  gnupg
-  libfido2
-  # sops
-
-  # Cloud-related tools and SDKs
+  # Container & Cloud Native
   docker
   docker-compose
   lazydocker
 
-  # Media-related packages
-  # emacs-all-the-icons-fonts
-  dejavu_fonts
+  # Security & Cryptography
+  age
+  age-plugin-yubikey
+  gnupg
+  libfido2
+
+  # Fonts & UI Appearance
   # https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/fonts/nerd-fonts/manifests/fonts.json
-  nerd-fonts."ubuntu-mono"
+  nerd-fonts."jetbrains-mono"
   nerd-fonts."fira-code"
   nerd-fonts."fira-mono"
-  # ffmpeg
-  fd
-  font-awesome
-  hack-font
-  noto-fonts
-  noto-fonts-emoji
-  meslo-lgs-nf
-
-  # # Node.js development tools
-  # nodePackages.npm # globally install npm
-  # nodePackages.prettier
-  # nodejs
-  uv
-
-  # Text and terminal utilities
-  htop
-  # hunspell
-  iftop
-  jetbrains-mono
-  jq
-  ripgrep
-  tree
-  tmux
-  unrar
-  unzip
-  p7zip
   zsh-powerlevel10k
-  # exa
-  duf
-  difftastic
+  # dejavu_fonts
+  # font-awesome
+  # hack-font
+  # noto-fonts
+  # noto-fonts-emoji
+  # meslo-lgs-nf
+  # jetbrains-mono
 
-  jq # A lightweight and flexible command-line JSON processor
-  yq-go # yaml processer https://github.com/mikefarah/yq
-  fzf # A command-line fuzzy finder
-  localsend
-  xsel
-  yazi
-  yaziPlugins.rich-preview # Rich preview for Yazi
-  # rich-cli            # Rich command-line interface for rich preview
-  obsidian
-  # microsoft-edge
-  #drawio # install by homebrew, pkgs in nix is too old
-  zotero
-  zoxide
+  # Apps
+  tmux
   vscode
   neovim
+  yazi
+  yaziPlugins.rich-preview
+  # rich-cli            # Rich command-line interface for rich preview
+  obsidian
+  zotero
+  localsend
+
 ]
